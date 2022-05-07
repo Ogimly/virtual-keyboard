332043f7f43af2215aa1cba0c47534c60d63ea69
ogimly <ogimly@gmail.com>
2022-05-08 02:41:21 +0500

    refactor: add forEach to arrays

--- src/scripts/KeyboardClass.js
+++ src/scripts/KeyboardClass.js
@@ -31,65 +31,64 @@ export default class Keyboard {
   static generateKeysArray(data) {
     const resArray = [];
 
-    for (let row = 0; row < data.length; row += 1) {
-      const arr = data[row];
+    data.forEach((row) => {
       const newArr = [];
 
-      for (let col = 0; col < arr.length; col += 1) {
-        newArr.push(new Key(arr[col]));
-      }
+      row.forEach((col) => {
+        newArr.push(new Key(col));
+      });
 
       resArray.push(newArr);
-    }
+    });
+
     return resArray;
   }
 
   // add keys to DOM - in keyboardWrapper
   addKeysToDOM(keyboardWrapper) {
-    for (let row = 0; row < this.keysArray.length; row += 1) {
+    this.keysArray.forEach((row) => {
       const keyboardRow = document.createElement('div');
       keyboardRow.classList.add('keyboard_row');
 
-      const arr = this.keysArray[row];
-      for (let col = 0; col < arr.length; col += 1) {
-        const newKey = arr[col].createKeyToDOM(this.lang, this.caseStatus(row));
+      row.forEach((key) => {
+        const newKey = key.createKeyToDOM(this.lang, this.caseStatus(this.lang, key));
 
         keyboardRow.append(newKey);
-      }
+      });
 
       keyboardWrapper.append(keyboardRow);
-    }
+    });
   }
 
   // update text on keys in DOM
   updateKeysInDOM() {
     let keyDOM = null;
-    for (let row = 0; row < this.keysArray.length; row += 1) {
-      const arr = this.keysArray[row];
 
-      for (let col = 0; col < arr.length; col += 1) {
-        keyDOM = arr[col].keyDOM;
-        keyDOM.firstChild.textContent = arr[col][this.lang][this.caseStatus(row)];
-      }
-    }
+    this.keysArray.forEach((row) => {
+      row.forEach((key) => {
+        keyDOM = key.keyDOM;
+        keyDOM.firstChild.textContent = key[this.lang][this.caseStatus(this.lang, key)];
+      });
+    });
   }
 
   // return low or up case depend of capsLockOn & shiftOn
-  caseStatus(row) {
+  caseStatus(lang, key) {
     let res = '';
 
-    // for digits caps dont work
-    if (row === 0)
-      if (this.ShiftLeftOn || this.ShiftRightOn) {
-        res = 'up';
-      } else {
+    const capsAllow = key[lang].caps;
+
+    if (capsAllow)
+      if (this.capsLockOn === (this.ShiftLeftOn || this.ShiftRightOn)) {
         res = 'low';
+      } else {
+        res = 'up';
       }
-    // for other rows
-    else if (this.capsLockOn === (this.ShiftLeftOn || this.ShiftRightOn)) {
-      res = 'low';
-    } else {
+    // for digits & some symbols caps dont work
+    else if (this.ShiftLeftOn || this.ShiftRightOn) {
       res = 'up';
+    } else {
+      res = 'low';
     }
 
     return res;
@@ -99,16 +98,13 @@ export default class Keyboard {
   findKeyOnCode(code) {
     let res = null;
 
-    for (let row = 0; row < this.keysArray.length; row += 1) {
-      const arr = this.keysArray[row];
-
-      for (let col = 0; col < arr.length; col += 1) {
-        if (arr[col].id === code) {
-          res = arr[col];
-          break;
+    this.keysArray.forEach((row) => {
+      row.forEach((key) => {
+        if (key.id === code) {
+          res = key;
         }
-      }
-    }
+      });
+    });
     return res;
   }
 
